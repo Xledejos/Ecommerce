9e46beec29ae3681453f623697dbfc2dc7eadf95
Omar Ledesma <joseomar.ledagui@gmail.com>
2023-11-11 01:42:37 +0100

    IGeneric Repository

---
+++ wallapop.AccessData/Repository/IRepository/IGenericRepository.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wallapop.AccessData.Repository.IRepository
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        Task<T?> GetById(int id);
+        Task<IEnumerable<T>> GetAll(
+            Expression<Func<T, bool>> filtro = null,
+            Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null,
+            string includeProperties = null,
+            bool isTracking = true
+            );
+
+        Task<T> GetFirst(
+            Expression<Func<T, bool>> filtro = null,
+            string includeProperties = null,
+            bool isTracking = true
+            );
+
+        Task Insert(T entity);
+        void Remove(T entity);
+        void RemoveRange(IEnumerable<T> entity);
+    }
+}
